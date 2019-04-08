8b32f11329108b21857fe7137d9c33dee8c2d569
Aleum <aleum@Katrinas-MacBook-Pro.local>
2019-04-08 17:18:31 +0200

    jwt

---
+++ myapp/middleware/checktoken.js
@@ -0,0 +1,37 @@
+const jwt = require('jsonwebtoken');
+
+module.exports = {
+    checkTokenForRoles: requiredRoles => {
+        return function(req, res, next) {
+            const header = req.headers['authorization'];
+        
+            if (typeof header !== 'undefined') {
+                const bearer = header.split(' ');
+                const token = bearer[1];
+                
+
+                try {
+                    const user = jwt.verify(token, 'secretKey'); 
+                    let found = false;
+                    requiredRoles.split(',').map(e => {
+                        if (user.role === e ) {
+                            found = true;
+                        }
+                    });
+                    if (!found) {
+                        console.log('user does not have one of required roles: ', requiredRoles);
+                        return res.sendStatus(403); 
+                    }
+                    return next();
+                    
+                } catch (err) {
+                    console.log(err);
+                    res.sendStatus(403);
+                }
+            } else {
+                console.log('Request is missing \'authorization\' header');
+                res.sendStatus(403);
+            }
+        }
+    }
+}
--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -1,13 +1,36 @@
 var express = require('express');
 var router = express.Router();
 const axios = require('axios');
+const jwt = require('jsonwebtoken');
+const { checkTokenForRoles } = require('../middleware/checktoken');
 const clientsData = 'http://www.mocky.io/v2/5808862710000087232b75ac';
 const policiesData = 'http://www.mocky.io/v2/580891a4100000e8242b75c5';
 
 
 
+// generate JWT token
+router.get('/api/v1/users/token/:userid', function(req, res, next) {
+  console.log('hihi');
+  axios.get(clientsData) 
+    .then(results => {
+      if (results.error) {
+        res.status(500).send(results.error);
+      } else {
+        for ( let i = 0; i < results.data.clients.length; i++ ) {
+          if (results.data.clients[i].id === req.params.userid) {
+            res.send(jwt.sign(results.data.clients[i], 'secretKey', { expiresIn: '1h' }));
+          }
+        }
+        console.log('ERROR: cannot login');
+        res.status(404).send('404 Error: No user found for that id');
+        
+      }
+    });
+});
+
 // Get user data filtered by userid or username OK
-router.get('/api/v1/users/', function(req, res, next) {
+router.get('/api/v1/users/', checkTokenForRoles('user,admin'), function(req, res, next) {
+  console.log('test')
   let idParam = req.query.userid;
   let nameParam = req.query.username;
   axios.get(clientsData)
@@ -79,6 +102,7 @@ router.get('/api/v1/policies', function(req, res, next) {
 // Get the user linked to a policy number OK
 router.get('/api/v1/users/:policyId', function(req, res, next) {
   let userId = '';
+  let found = false;
   axios.get(policiesData)
     .then(results1 => {
       if (results1.error) {
